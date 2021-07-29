5d1df6a94824c285b400f4204f4b11ea2c385547
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2021-07-29 21:15:12 +0500

    Add reactive forms support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
@@ -7,16 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IssueListComponent } from './issue-list/issue-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    IssueListComponent
-  ],
+  declarations: [AppComponent, IssueListComponent],
   imports: [
     BrowserModule,
     ClarityModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
