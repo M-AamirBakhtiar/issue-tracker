9c567700c20b523790a4c1e648fa034c4adf27f6
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2021-07-29 21:32:15 +0500

    Add clarity markup and form builder to issue report component

--- src/app/issue-report/issue-report.component.ts
+++ src/app/issue-report/issue-report.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-issue-report',
   templateUrl: './issue-report.component.html',
-  styleUrls: ['./issue-report.component.css']
+  styleUrls: ['./issue-report.component.css'],
 })
 export class IssueReportComponent implements OnInit {
+  issueForm: FormGroup | undefined;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.issueForm = this.fb.group({
+      title: [''],
+      description: [''],
+      priority: [''],
+      type: [''],
+    });
   }
-
 }
