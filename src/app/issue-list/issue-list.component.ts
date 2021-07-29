14e23b96f485d901979ecc7d43740b567fd8a72a
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2021-07-29 20:49:17 +0500

    Add issue service to issue component

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Issue } from '../issue';
+import { IssuesService } from '../issues.service';
 
 @Component({
   selector: 'app-issue-list',
   templateUrl: './issue-list.component.html',
-  styleUrls: ['./issue-list.component.css']
+  styleUrls: ['./issue-list.component.css'],
 })
 export class IssueListComponent implements OnInit {
+  issues: Issue[] = [];
 
-  constructor() { }
+  constructor(private issueService: IssuesService) {}
 
   ngOnInit(): void {
+    this.getIssues();
   }
 
+  private getIssues(): void {
+    this.issues = this.issueService.getPendingIssues();
+  }
 }
